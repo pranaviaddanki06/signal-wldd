ea9fc88f0c2664a638c33681d5e433d5de4d9a6b
pranaviaddanki06 <pranaviaddanki.15@gmail.com>
2026-08-18 09:37:01 +0530

    Add final SIGNAL presentation shell

---
+++ app/final/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+import Home from '../page';
+import LaunchOverlay from '../LaunchOverlay';
+import ThemeToggle from '../ThemeToggle';
+import './polish.css';
+export default function FinalExperience(){return <><LaunchOverlay/><ThemeToggle/><Home/></>}
